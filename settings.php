592c2436f019dff8b20db661c061845f363c4062
Atreya Goswami <atreya@iitk.ac.in>
2022-04-03 16:29:41 +0530

    Updated user settings page
    Co-authored-by: Lipi Jain <lipijain2@users.noreply.github.com>

--- settings.php
+++ settings.php
@@ -8,7 +8,7 @@
 <!DOCTYPE html>
 <html>
   <head>
-		<title>Lifestyle Store</title>
+		<title>Amazing Zone</title>
 		<meta charset="utf-8">
 		<meta name='viewport' content="width=device-width, initial-scale=1.0">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -24,62 +24,153 @@
 
 		<?php include 'includes/header.php'; ?>
 
-		<div class="container container-modi">
-			<div class="row row-style">
-				<div class="col-xs-offset-2 col-xs-8 col-md-offset-3 col-md-6">
-                                <?php if(isset($_GET['success_message'])) {
-                                    echo"<div class='panel panel-success'>
-						<div class='panel-heading'>
-							<h4>Success!</h4>
-						</div>
-						<div class='panel-body'>
-							<p>".$_GET['success_message']."</p>
-						</div></div>"; 
-                                    
-                                }?>
-                                
-                                <?php if(isset($_GET['password_error'])) {
-                                    echo"<div class='panel panel-warning'>
-						<div class='panel-heading'>
-							<h4>Error!</h4>
-						</div>
-						<div class='panel-body'>
-							<p>".$_GET['password_error']."</p>
-						</div></div>"; 
-                                    
-                                }?>
-                                    
-                                <?php if(isset($_GET['passwordmatch_error'])) {
-                                    echo"<div class='panel panel-warning'>
-						<div class='panel-heading'>
-							<h4>Error!</h4>
-						</div>
-						<div class='panel-body'>
-							<p>".$_GET['passwordmatch_error']."</p>
-						</div></div>"; 
-                                    
-                                }?>
-                                    <form action="settings_script.php" method="post">
-					<h2>Change Password</h2>
-					<div class="form-group">
-                                            <input type="password" name="oldpassword" placeholder="Old Password" class="form-control" required="True">
-					</div>
-					<div class="form-group">
-                                            <input type="password" name="newpassword" placeholder="New Password" class="form-control" pattern=".{6,}" required="True">
-					</div>
-					<div class="form-group">
-                                            <input type="password" name="renewpassword" placeholder="Retype New Password" class="form-control" pattern=".{6,}" required="True">
-					</div>
-					<div class="form-group">
-						<input type="submit" value="Change Password" class="btn btn-primary">
-					</div>
-                                    </form>
-				</div>
-			</div>
+        <div class="container-fluid">
+    <div class="row">
+      <main class="col-md-9 ml-sm-auto col-lg-12 px-md-4 py-4">
+        <nav aria-label="breadcrumb">
+          <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="#">Home</a></li>
+            <li class="breadcrumb-item active" aria-current="page">New Form</li>
+          </ol>
+        </nav>
+        <h1 style="margin-top:50px;">User Profile Page</h1>
+        <section>
+          <div class="container">
+            <h4 style="line-height: 50px;"><strong>Details of User account</strong></h4>
+            <?php
+                $user_id = $_SESSION['user_id'];
+                $select_user_query = "SELECT * FROM users WHERE user_id = $user_id";
+                $select_user_query_result = mysqli_query($con , $select_user_query) or die(mysqli_error($con));
+                $user_row = mysqli_fetch_array($select_user_query_result);
+            ?>
+            <form style="margin-top: 20px;" method="post" action="settings-script.php">
+                <div class="form-row">
+                    <div class="form-group col-md-4">
+                        <label for="name">Full Name</label>
+                        <input type="text" class="form-control" id="name" value="<?php echo $user_row['name'] ?>" disabled>
+                    </div>
+                    <div class="form-group col-md-offset-2 col-md-4">
+                        <label for="email">Email ID</label>
+                        <input type="email" class="form-control" id="email" value="<?php echo $user_row['email'] ?>" disabled>
+                    </div>
+                </div>
+                <div class="form-row">
+                    <?php if($_SESSION['role'] == 0) { 
+                        $customer_id = $_SESSION['specific_id'];
+                        $select_customer_query = "SELECT * FROM customers WHERE customer_id = $customer_id";
+                        $select_customer_query_result = mysqli_query($con , $select_customer_query) or die(mysqli_error($con));
+                        $customer_row = mysqli_fetch_array($select_customer_query_result);
+                    ?>
+                        <div class="form-group col-md-4">
+                            <label for="contact">Contact Number</label>
+                            <input type="text" class="form-control" id="contact" value="<?php echo $customer_row['contact_no']; ?>" disabled>
+                        </div>
+                        <div class="form-group col-md-4">
+                            <label for="city">City</label>
+                            <input type="text" class="form-control" id="city" value="<?php echo $customer_row['city']; ?>" disabled>
+                        </div>
+                        <div class="form-group col-md-4">
+                            <label for="country">Country</label>
+                            <input type="text" class="form-control" id="country" value="<?php echo $customer_row['country']; ?>" disabled>
+                        </div>
+                    </div>
+                    <div class="form-row">
+                        <div class="form-group col-md-12">
+                            <label for="address">Address</label>
+                            <input type="text" class="form-control" id="address" value="<?php echo $customer_row['address']; ?>" disabled>
+                        </div>
+                    </div>
                     <div class="push"></div>
-		</div>
+                    <div class="form-row" id="edit-row">
+                        <div class="form-group col-md-offset-5 col-md-2" id="edit">
+                            <div class="btn btn-primary" onclick="editCustomer()">Edit Details</div>
+                        </div>
+                    </div>
+                    <?php } else if($_SESSION['role'] == 1) { 
+                        $supplier_id = $_SESSION['specific_id'];
+                        $select_supplier_query = "SELECT * FROM suppliers WHERE supplier_id = $supplier_id";
+                        $select_supplier_query_result = mysqli_query($con , $select_supplier_query) or die(mysqli_error($con));
+                        $supplier_row = mysqli_fetch_array($select_supplier_query_result);
+                    ?>
+                        <div class="form-group col-md-4">
+                            <label for="contact">Contact Number</label>
+                            <input type="text" class="form-control" id="contact" value="<?php echo $supplier_row['contact_no']; ?>" disabled>
+                        </div>
+                        <div class="form-group col-md-offset-2 col-md-4">
+                            <label for="license">License ID</label>
+                            <input type="text" class="form-control" id="license" value="<?php echo $supplier_row['license']; ?>" disabled>
+                        </div>
+                    </div>
+                    <div class="form-row">
+                        <div class="form-group col-md-12">
+                            <label for="address">Address</label>
+                            <input type="text" class="form-control" id="address" value="<?php echo $supplier_row['address']; ?>" disabled>
+                        </div>
+                    </div>
+                    <div class="push"></div>
+                    <div class="form-row" id="edit-row">
+                        <div class="form-group col-md-offset-5 col-md-2" id="edit">
+                            <div class="btn btn-primary" onclick="editSupplier()">Edit Details</div>
+                        </div>
+                    </div>
+                    <?php } ?>
+                </form>
+            </div>
+        </section>
+        </main>
+        </div>
+    </div>
 		
-
-		<?php include 'includes/footer.php'; ?>
+        <div class = "navbar navbar-inverse navbar-fixed-bottom">
+            <?php include 'includes/footer.php'; ?>
+        </div>
+        <script>
+            function editCustomer() {
+                document.getElementById("address").disabled = false;
+                document.getElementById("contact").disabled = false;
+                document.getElementById("city").disabled = false;
+                document.getElementById("country").disabled = false;
+                document.getElementById("name").disabled = false;
+                document.getElementById("email").disabled = false;
+                $( "#edit" ).remove();
+                newRow = 
+                '<div class="form-group col-md-offset-5 col-md-1" id="submit"><input type="submit" value="Update" class="btn btn-primary"></div><div class="form-group col-md-1" id="cancel"><div class="btn btn-danger" onclick="restoreCustomer()">Cancel</div></div>';
+                $('#edit-row').append(newRow);
+            }
+            function editSupplier() {
+                document.getElementById("address").disabled = false;
+                document.getElementById("contact").disabled = false;
+                document.getElementById("license").disabled = false;
+                document.getElementById("name").disabled = false;
+                document.getElementById("email").disabled = false;
+                $( "#edit" ).remove();
+                newRow = 
+                '<div class="form-group col-md-offset-5 col-md-1" id="submit"><input type="submit" value="Update" class="btn btn-primary"></div><div class="form-group col-md-1" id="cancel"><div class="btn btn-danger" onclick="restoreSupplier()">Cancel</div></div>';
+                $('#edit-row').append(newRow);
+            }
+            function restoreCustomer() {
+                document.getElementById("address").disabled = true;
+                document.getElementById("contact").disabled = true;
+                document.getElementById("city").disabled = true;
+                document.getElementById("country").disabled = true;
+                document.getElementById("name").disabled = true;
+                document.getElementById("email").disabled = true;
+                $( "#submit" ).remove();
+                $( "#cancel" ).remove();
+                newRow = '<div class="form-group col-md-offset-5 col-md-2" id="edit"><div class="btn btn-primary" onclick="editCustomer()">Edit Details</div></div>';
+                $('#edit-row').append(newRow);
+            }
+            function restoreSupplier() {
+                document.getElementById("address").disabled = true;
+                document.getElementById("contact").disabled = true;
+                document.getElementById("license").disabled = true;
+                document.getElementById("name").disabled = true;
+                document.getElementById("email").disabled = true;
+                $( "#submit" ).remove();
+                $( "#cancel" ).remove();
+                newRow = '<div class="form-group col-md-offset-5 col-md-2" id="edit"><div class="btn btn-primary" onclick="editSupplier()">Edit Details</div></div>';
+                $('#edit-row').append(newRow);
+            }
+        </script>
 	</body>
 </html>
