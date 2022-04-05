c578c916f1f7cac622af0278bcabb21e93f710ed
Atreya Goswami <atreya@iitk.ac.in>
2022-04-05 17:41:41 +0530

    Hyperlinks in Product Categories Page Fixes #1

--- product-categories.php
+++ product-categories.php
@@ -32,59 +32,59 @@
 			
                     <div class="row text-center">
 				<div class="col-md-3 col-sm-6">
-					<div class="thumbnail">
-                        <img src="images/1.jpg" alt="" class="img-responsive">
-                        <div class="caption">
-							<a href = "products.php?cat=1">
-							<h3>Automobile</h3>
-							<?php if (!isset($_SESSION['email'])) { ?> 
-							<p><a href="login.php" role="button" class="btn btn-primary btn-block">Buy Now</a></p> 
-							<?php } ?>
-							</a>
-						</div>
-					</div>
+                    <a href = "products.php?cat=1">
+                        <div class="thumbnail">
+                            <img src="images/1.jpg" alt="" class="img-responsive">
+                            <div class="caption">
+                                <h3>Automobile</h3>
+                                <?php if (!isset($_SESSION['email'])) { ?> 
+                                <p><a href="login.php" role="button" class="btn btn-primary btn-block">Buy Now</a></p> 
+                                <?php } ?>
+                            </div>
+                        </div>
+                    </a>
 				</div>
 
 				<div class="col-md-3 col-sm-6">
-					<div class="thumbnail">
-						<img src="images/2.jpg" alt="" class="img-responsive">
-						<div class="caption">
-							<a href = "products.php?cat=2">
-							<h3>Beauty Health & Fitness</h3>
-							<?php if (!isset($_SESSION['email'])) { ?> 
-							<p><a href="login.php" role="button" class="btn btn-primary btn-block">Buy Now</a></p> 
-							<?php } ?>
-							</a>
-						</div>
-					</div>
+                    <a href = "products.php?cat=2">
+                        <div class="thumbnail">
+                            <img src="images/2.jpg" alt="" class="img-responsive">
+                            <div class="caption">
+                                <h3>Beauty Health & Fitness</h3>
+                                <?php if (!isset($_SESSION['email'])) { ?> 
+                                <p><a href="login.php" role="button" class="btn btn-primary btn-block">Buy Now</a></p> 
+                                <?php } ?>
+                            </div>
+                        </div>
+                    </a>
 				</div>
 
 				<div class="col-md-3 col-sm-6">
-					<div class="thumbnail">
-						<img src="images/3.jpg" alt="" class="img-responsive">
-						<div class="caption">
-							<a href = "products.php?cat=3">
-							<h3>Books</h3>
-							<?php if (!isset($_SESSION['email'])) { ?> 
-							<p><a href="login.php" role="button" class="btn btn-primary btn-block">Buy Now</a></p> 
-							<?php } ?>
-							</a>
-						</div>
-					</div>
+                    <a href = "products.php?cat=3">
+                        <div class="thumbnail">
+                            <img src="images/3.jpg" alt="" class="img-responsive">
+                            <div class="caption">
+                                <h3>Books</h3>
+                                <?php if (!isset($_SESSION['email'])) { ?> 
+                                <p><a href="login.php" role="button" class="btn btn-primary btn-block">Buy Now</a></p> 
+                                <?php } ?>
+                            </div>
+                        </div>
+                    </a>
 				</div>
 
 				<div class="col-md-3 col-sm-6">
-					<div class="thumbnail">
-						<img src="images/4.jpg" alt="" class="img-responsive">
-						<div class="caption">
-							<a href = "products.php?cat=4">
-							<h3>Drinks</h3>
-							<?php if (!isset($_SESSION['email'])) { ?> 
-							<p><a href="login.php" role="button" class="btn btn-primary btn-block">Buy Now</a></p> 
-							<?php } ?>
-							</a>
-						</div>
-					</div>
+                    <a href = "products.php?cat=4">
+                        <div class="thumbnail">
+                            <img src="images/4.jpg" alt="" class="img-responsive">
+                            <div class="caption">
+                                <h3>Drinks</h3>
+                                <?php if (!isset($_SESSION['email'])) { ?> 
+                                <p><a href="login.php" role="button" class="btn btn-primary btn-block">Buy Now</a></p> 
+                                <?php } ?>
+                            </div>
+                        </div>
+                    </a>
 				</div>
                 </div>
 
@@ -92,117 +92,118 @@
 
                 <div class="row text-center">
 				<div class="col-md-3 col-sm-6">
-					<div class="thumbnail">
-						<img src="images/5.jpg" alt="" class="img-responsive">
-						<div class="caption">
-							<a href = "products.php?cat=5">
-							<h3>Electronics</h3>
-							<?php if (!isset($_SESSION['email'])) { ?> 
-							<p><a href="login.php" role="button" class="btn btn-primary btn-block">Buy Now</a></p> 
-							<?php } ?>
-							</a>
-						</div>
-					</div>
+                    <a href = "products.php?cat=5">
+                        <div class="thumbnail">
+                            <img src="images/5.jpg" alt="" class="img-responsive">
+                            <div class="caption">
+                                <h3>Electronics</h3>
+                                <?php if (!isset($_SESSION['email'])) { ?> 
+                                <p><a href="login.php" role="button" class="btn btn-primary btn-block">Buy Now</a></p> 
+                                <?php } ?>
+                            </div>
+                        </div>
+                    </a>
 				</div>
 
 				<div class="col-md-3 col-sm-6">
-					<div class="thumbnail">
-						<img src="images/6.jpg" alt="" class="img-responsive">
-						<div class="caption">
-						<a href = "products.php?cat=6">
-							<h3>Food</h3>
-							<?php if (!isset($_SESSION['email'])) { ?> 
-							<p><a href="login.php" role="button" class="btn btn-primary btn-block">Buy Now</a></p> 
-							<?php } ?>
-						</a>
-						</div>
-					</div>
+                    <a href = "products.php?cat=6">	
+                        <div class="thumbnail">
+                            <img src="images/6.jpg" alt="" class="img-responsive">
+                            <div class="caption">
+                                <h3>Food</h3>
+                                <?php if (!isset($_SESSION['email'])) { ?> 
+                                <p><a href="login.php" role="button" class="btn btn-primary btn-block">Buy Now</a></p> 
+                                <?php } ?>
+                            </div>
+					    </div>
+                    </a>
 				</div>
 
 				<div class="col-md-3 col-sm-6">
-					<div class="thumbnail">
-						<img src="images/7.jpg" alt="" class="img-responsive">
-						<div class="caption">
-						<a href = "products.php?cat=7">
-							<h3>Home</h3>
-							<?php if (!isset($_SESSION['email'])) { ?> 
-							<p><a href="login.php" role="button" class="btn btn-primary btn-block">Buy Now</a></p> 
-							<?php } ?>
-						</a>
-						</div>
-					</div>
+				    <a href = "products.php?cat=7">	
+                        <div class="thumbnail">
+						    <img src="images/7.jpg" alt="" class="img-responsive">
+						    <div class="caption">
+                                <h3>Home</h3>
+		    					<?php if (!isset($_SESSION['email'])) { ?> 
+			    				<p><a href="login.php" role="button" class="btn btn-primary btn-block">Buy Now</a></p> 
+			    				<?php } ?>
+						    </div>
+					    </div>
+                    </a>
 				</div>
 
 				<div class="col-md-3 col-sm-6">
-					<div class="thumbnail">
-						<img src="images/8.jpg" alt="" class="img-responsive">
-						<div class="caption">
-						<a href = "products.php?cat=8">
-							<h3>Kids' Fashion</h3>
-							<?php if (!isset($_SESSION['email'])) { ?> 
-							<p><a href="login.php" role="button" class="btn btn-primary btn-block">Buy Now</a></p> 
-							<?php } ?>
-						</a>
-						</div>
-					</div>
+                    <a href = "products.php?cat=8">	
+                        <div class="thumbnail">
+                            <img src="images/8.jpg" alt="" class="img-responsive">
+                            <div class="caption">
+                                <h3>Kids' Fashion</h3>
+                                <?php if (!isset($_SESSION['email'])) { ?> 
+                                <p><a href="login.php" role="button" class="btn btn-primary btn-block">Buy Now</a></p> 
+                                <?php } ?>
+                            </div>
+                        </div>
+                    </a>
 				</div>
                     </div>
 
                     <div class="row text-center">
 				<div class="col-md-3 col-sm-6">
-					<div class="thumbnail">
-						<img src="images/9.jpg" alt="" class="img-responsive">
-						<div class="caption">
-						<a href = "products.php?cat=9">
-							<h3>Men's Fashion</h3>
-							<?php if (!isset($_SESSION['email'])) { ?> 
-							<p><a href="login.php" role="button" class="btn btn-primary btn-block">Buy Now</a></p> 
-							<?php } ?>
-						</a>
-						</div>
-					</div>
+                    <a href = "products.php?cat=9">	
+                        <div class="thumbnail">
+                            <img src="images/9.jpg" alt="" class="img-responsive">
+                            <div class="caption">
+                                <h3>Men's Fashion</h3>
+                                <?php if (!isset($_SESSION['email'])) { ?> 
+                                <p><a href="login.php" role="button" class="btn btn-primary btn-block">Buy Now</a></p> 
+                                <?php } ?>
+                            </div>
+                        </div>
+                    </a>
 				</div>
 
 				<div class="col-md-3 col-sm-6">
-					<div class="thumbnail">
-						<img src="images/10.jpg" alt="" class="img-responsive">
-						<div class="caption">
-						<a href = "products.php?cat=10">
-							<h3>Sports</h3>
-							<?php if (!isset($_SESSION['email'])) { ?> 
-							<p><a href="login.php" role="button" class="btn btn-primary btn-block">Buy Now</a></p> 
-							<?php } ?>
-						</a>
-						</div>
-					</div>
+                    <a href = "products.php?cat=10">
+                        <div class="thumbnail">
+                            <img src="images/10.jpg" alt="" class="img-responsive">
+                            <div class="caption">
+                            
+                                <h3>Sports</h3>
+                                <?php if (!isset($_SESSION['email'])) { ?> 
+                                <p><a href="login.php" role="button" class="btn btn-primary btn-block">Buy Now</a></p> 
+                                <?php } ?>
+                            </div>
+                        </div>
+                    </a>
 				</div>
 
 				<div class="col-md-3 col-sm-6">
-					<div class="thumbnail">
-						<img src="images/11.jpg" alt="" class="img-responsive">
-						<div class="caption">
-						<a href = "products.php?cat=11">
-							<h3>Toys & Baby Products</h3>
-							<?php if (!isset($_SESSION['email'])) { ?> 
-							<p><a href="login.php" role="button" class="btn btn-primary btn-block">Buy Now</a></p> 
-							<?php } ?>
-						</a>
-						</div>
-					</div>
+                    <a href = "products.php?cat=11">
+                        <div class="thumbnail">
+                            <img src="images/11.jpg" alt="" class="img-responsive">
+                            <div class="caption">
+                            <h3>Toys & Baby Products</h3>
+                            <?php if (!isset($_SESSION['email'])) { ?> 
+                            <p><a href="login.php" role="button" class="btn btn-primary btn-block">Buy Now</a></p> 
+                            <?php } ?>
+                            </div>
+                        </div>
+                    </a>
 				</div>
 
 				<div class="col-md-3 col-sm-6">
-					<div class="thumbnail">
-						<img src="images/12.jpg" alt="" class="img-responsive">
-						<div class="caption">
-						<a href = "products.php?cat=12">
-							<h3>Women's Fashion</h3>
-							<?php if (!isset($_SESSION['email'])) { ?> 
-							<p><a href="login.php" role="button" class="btn btn-primary btn-block">Buy Now</a></p> 
-							<?php } ?>
-						</a>
-						</div>
-					</div>
+                    <a href = "products.php?cat=12">
+                        <div class="thumbnail">
+                            <img src="images/12.jpg" alt="" class="img-responsive">
+                            <div class="caption">
+                                <h3>Women's Fashion</h3>
+                                <?php if (!isset($_SESSION['email'])) { ?> 
+                                <p><a href="login.php" role="button" class="btn btn-primary btn-block">Buy Now</a></p> 
+                                <?php } ?>
+                            </div>
+                        </div>
+                    </a>
 				</div>
                 </div>
                 </div>
